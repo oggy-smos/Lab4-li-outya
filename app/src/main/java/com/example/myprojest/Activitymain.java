8a5eaabc31808dee9f968669b95564a74a60b849
Your Name <you@example.com>
2025-10-16 23:58:07 +0700

    first commit

---
+++ app/src/main/java/com/example/myprojest/Activitymain.java
@@ -0,0 +1,4 @@
+package com.example.myprojest;
+
+public class Activitymain {
+}
--- app/src/main/java/com/example/myprojest/ConstraintLayoutActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.myprojest;
-
-import android.app.Activity;
-
-public class ConstraintLayoutActivity extends Activity {
-}
